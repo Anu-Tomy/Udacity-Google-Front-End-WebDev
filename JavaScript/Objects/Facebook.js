f48cb092316bf765126d251b84798d4631ca7ba4
Anu Tomy <anuatomy@gmail.com>
2018-03-16 16:54:56 +0000

    Complete the Challenge

--- JavaScript/Objects/Facebook.js
+++ JavaScript/Objects/Facebook.js
@@ -1,9 +1,9 @@
 /* Directions:
 Create an object called facebookProfile. The object should have 3 properties:
 
-your name
-the number of friends you have, and
-an array of messages you've posted (as strings)
+-your name
+-the number of friends you have, and
+-an array of messages you've posted (as strings)
 The object should also have 4 methods:
 
 postMessage(message) - adds a new message string to the array
